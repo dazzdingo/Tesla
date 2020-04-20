b1a932e7da399a5b590e33e483da4723e919d976
D 1 26 26 <38295166+dazzdingo@users.noreply.github.com>
2020-04-20 20:56:05 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
-
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 const int maxx = 15;
@@ -8,43 +9,104 @@ bool GameOver;
 const int width = maxx;
 const int height = maxx;
 int x, y, YugoX, YugoY, PintoX,PintoY, ElonX, ElonY, PrizeX,PrizeY;
-enum eDirection {Stop = 0, left, right, up, down};
+enum eDirection {Stop = 0, Left, Right, Up, Down};
+eDirection dir;
 
 void SetUp(){
     GameOver = false;
-    x = rand() % width;
-    y = rand() % height;
-    YugoX = rand() % width;
-    YugoY = rand() % height;
-    PintoX = rand() % width;
-    PintoY = rand() % height;
-    ElonX = rand() % width;
-    ElonY = rand() % height;
-    PrizeX = rand() % width;
-    PrizeY = rand() % height;
-    
+    dir = Stop;
+    x = rand() % width + 1;
+    y = rand() % height + 1;
+    YugoX = rand() % width +1;
+    YugoY = rand() % height +1;
+    PintoX = rand() % width +1;
+    PintoY = rand() % height +1;
+    ElonX = rand() % width +1;
+    ElonY = rand() % height +1;
+    PrizeX = rand() % width +1;
+    PrizeY = rand() % height +1;
 }
 
 void Draw(){
-    
-    
+    system("cls"); //clearscreen
+    for (int i = 0; i < width + 2; i++)
+        cout << "x";
+    cout << endl;
+    for (int i = 0; i < width + 2; i++){
+        for (int j = 0; j <= height + 1; j++){
+            if (j == 0){
+                cout << "x";
+            }
+            if (j == width ) {
+                cout << "x";
+            }
+            if (i == y && j == x){
+                cout << "O";
+            }
+            else {
+                cout << " ";
+            }
+
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < width + 2; i++)
+        cout << "x";
+    cout << endl;
 }
 
 void Input(){
-    
-    
+    if (_kbhit()){
+        switch (_getch()) {
+        case 'w':
+            dir = Up;
+            break;
+        case 's':
+            dir = Down;
+            break;
+        case 'a':
+            dir = Left;
+            break;
+        case 'd':
+            dir = Right;
+            break;
+        case 'r':
+            GameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic(){
-    
-    
+    switch (dir) {
+    case Stop:
+        break;
+    case Up:
+        y--;
+        break;
+    case Down:
+        y++;
+        break;
+    case Left:
+        x--;
+        break;
+    case Right:
+        x++;
+        break;
+    default:
+        break;
+
+    }
+
 }
 int main (){
-    while (!GameOver) {
-        SetUp();
-        Draw();
-        Input();
-        Logic();        
-    }
-    return 0;
+srand(time(NULL));
+SetUp();
+while (!GameOver) {
+    Draw();
+    Input();
+    Logic();
+    Sleep(10);
+}
+return 0;
 }
